732a5f6bae1998e3c7a62710b67ce2584e74e606
Storm <87811650+Storm99999@users.noreply.github.com>
2023-06-20 22:30:35 +0200

    Update radar.lua

--- dependencies/radar.lua
+++ dependencies/radar.lua
@@ -77,6 +77,60 @@ UIAspectRatioConstraint_2.Parent = PlayerTemplate
 
 -- Scripts:
 
+local UserInputService = game:GetService("UserInputService")
+        local runService = (game:GetService("RunService"));
+
+        local gui = Background
+
+        local dragging
+        local dragInput
+        local dragStart
+        local startPos
+
+        function Lerp(a, b, m)
+            return a + (b - a) * m
+        end;
+
+        local lastMousePos
+        local lastGoalPos
+        local DRAG_SPEED = (8); -- // The speed of the UI darg.
+        function Update(dt)
+            if not (startPos) then return end;
+            if not (dragging) and (lastGoalPos) then
+                gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, lastGoalPos.X.Offset, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, lastGoalPos.Y.Offset, dt * DRAG_SPEED))
+                return 
+            end;
+
+            local delta = (lastMousePos - UserInputService:GetMouseLocation())
+            local xGoal = (startPos.X.Offset - delta.X);
+            local yGoal = (startPos.Y.Offset - delta.Y);
+            lastGoalPos = UDim2.new(startPos.X.Scale, xGoal, startPos.Y.Scale, yGoal)
+            gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, xGoal, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, yGoal, dt * DRAG_SPEED))
+        end;
+
+        gui.InputBegan:Connect(function(input)
+            if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+                dragging = true
+                dragStart = input.Position
+                startPos = gui.Position
+                lastMousePos = UserInputService:GetMouseLocation()
+
+                input.Changed:Connect(function()
+                    if input.UserInputState == Enum.UserInputState.End then
+                        dragging = false
+                    end
+                end)
+            end
+        end)
+
+        gui.InputChanged:Connect(function(input)
+            if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+                dragInput = input
+            end
+        end)
+
+        runService.Heartbeat:Connect(Update)
+
 local function JZJOV_fake_script() -- RadarGui.RadarScript 
 	local script = Instance.new('LocalScript', RadarGui)
 
